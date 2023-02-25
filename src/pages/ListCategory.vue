2405969a7d6175a3b3fac26d03d59b79fb7b5273
Israel <febuxost@gmail.com>
2023-02-25 11:11:20 -0500

    asdasd

--- src/pages/ListCategory.vue
+++ src/pages/ListCategory.vue
@@ -1,12 +1,29 @@
 <template>
-    <h1>Listado</h1>
+    <h1>Listado Categorías</h1>
+
+    <!-- <div v-for="c in categories" :key="c.id">
+        <p>{{ c.title }}</p>
+    </div> -->
+
+    <div v-if="categories">
+        <img :src="categories" alt="Imagen de categoría"/>
+    </div>
+    <div v-else>
+        <p>Cargando...</p>
+    </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            categories:[]
+        }
+    },
     mounted(){
         this.$axios.get("https://aws.random.cat/meow?ref=apilist.fun")
         .then((res)=>{
-            console.log(res.data)
+            console.log(res.data.file)
+            this.categories = res.data.file
         })
     }
 }
