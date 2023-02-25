24a154ce0ab05597aecd78cfb3b5509f70f20041
Israel <febuxost@gmail.com>
2023-02-25 09:54:47 -0500

    LAFNJSAJDKASND

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
+//AXIOS
+import axios from "axios"
+
+//RUTAS DE NAGEVACIÓN
+import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+Window.axios = axios
 app.use(router)
 app.mount('#app')
--- src/pages/ListCategory.vue
+++ src/pages/ListCategory.vue
@@ -1,6 +1,13 @@
 <template>
     <h1>Listado</h1>
-    <h1>Listado</h1>
-    <h1>Listado</h1>
-    <h1>Listado</h1>
 </template>
+<script>
+export default {
+    mounted(){
+        this.$axios.get("https://aws.random.cat/meow?ref=apilist.fun")
+        .then((res)=>{
+            console.log(res.data)
+        })
+    }
+}
+</script>
